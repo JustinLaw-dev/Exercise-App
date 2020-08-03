5348bc7bbb0303c311583eea563d05f8560e9767
Justin Law <justinlaw31@gmail.com>
2020-08-03 14:28:28 -0700

    user auth added
    added user auth sign up

--- public/app.js
+++ public/app.js
@@ -1,5 +1,35 @@
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// firebase.analytics();
 
-console.log(firebase);
+//Get elements
+const textEmail = document.getElementById("email");
+const textPassword = document.getElementById("password");
+const btnLogin = document.getElementById("btnLogin");
+const btnSignUp = document.getElementById("btnSignUp");
+
+//login event
+btnLogin.addEventListener("click", (e) => {
+  //get email and pass
+  //TODO: Check for real email address
+  const email = textEmail.value;
+  const pass = textPassword.value;
+  const auth = firebase.auth();
+
+  //Sign in
+  const promise = auth.signInWithEmailAndPassword(email, pass);
+  promise.catch((e) => console.log(e.message));
+});
+
+//sign up event
+btnSignUp.addEventListener("click", (e) => {
+  //get email and pass
+  //TODO: Check for real email address
+  const email = textEmail.value;
+  const pass = textPassword.value;
+  const auth = firebase.auth();
+
+  //Sign in
+  const promise = auth.createUserWithEmailAndPassword(email, pass);
+  promise.catch((e) => console.log(e.message));
+});
