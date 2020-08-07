153198a0541807090bdf4be16667a24786c101f7
JustinLaw-dev <52508947+JustinLaw-dev@users.noreply.github.com>
2020-08-06 17:13:28 -0700

    Began tabs
    Created tab menu with buttons, and tab panels. Laid out header with log out button.
    
    Began work on tabs script,

--- public/app.js
+++ public/app.js
@@ -1,5 +1,89 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA55DpRmm4DNgT8w_EJnLAzGmBC0qg0RB4",
+  authDomain: "los-alamitos-pt-exercise-app.firebaseapp.com",
+  databaseURL: "https://los-alamitos-pt-exercise-app.firebaseio.com",
+  projectId: "los-alamitos-pt-exercise-app",
+  storageBucket: "los-alamitos-pt-exercise-app.appspot.com",
+  messagingSenderId: "587818075107",
+  appId: "1:587818075107:web:782b5b32806bd55ad0d6d6",
+  measurementId: "G-J3TZX37KTF",
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
-console.log(firebase);
+//Get elements
+function login() {
+  const textEmail = document.getElementById("email");
+  const textPassword = document.getElementById("password");
+  const btnLogin = document.getElementById("btnLogin");
+  const btnSignUp = document.getElementById("btnSignUp");
+
+  //login event
+  btnLogin.addEventListener("click", (e) => {
+    //get email and pass
+    //TODO: Check for real email address
+    const email = textEmail.value;
+    const pass = textPassword.value;
+    const auth = firebase.auth();
+
+    //Sign in
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch((e) => console.log(e.message));
+  });
+
+  //sign up event
+  btnSignUp.addEventListener("click", (e) => {
+    //get email and pass
+    //TODO: Check for real email address
+    const email = textEmail.value;
+    const pass = textPassword.value;
+    const auth = firebase.auth();
+
+    //Sign in
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch((e) => console.log(e.message));
+  });
+}
+
+//add real time listener, checks to see if user exists
+firebase.auth().onAuthStateChanged((firebaseUser) => {
+  if (firebaseUser) {
+    console.log("logged in");
+    if (window.location.href === "http://127.0.0.1:5500/public/index.html") {
+      window.location.href = "http://127.0.0.1:5500/public/main.html";
+    }
+  } else {
+    console.log("not a user/not logged in");
+  }
+});
+
+if (window.location.href === "http://127.0.0.1:5500/public/index.html") {
+  console.log("correct page");
+  login();
+}
+/////
+// Main Page scripts
+//
+/////
+
+//Log out
+if (window.location.href === "http://127.0.0.1:5500/public/main.html") {
+  const btnLogOut = document.getElementById("btnLogOut");
+  btnLogOut.addEventListener("click", (e) => {
+    firebase
+      .auth()
+      .signOut()
+      .then(function () {
+        console.log("Signed out succesfully");
+        window.location.href = "index.html";
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
+
+//tabs on main
+  function openTab(event, tabName) {
+    
+  }
+}
